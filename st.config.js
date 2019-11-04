2b4bca115536719a5de59d8cb7178c882a52487d
mansi1 <m.mansi1@gmx.net>
2019-11-04 21:30:22 +0100

    fix(duplication) fix duplication in bundel script

--- st.config.js
+++ st.config.js
@@ -2,9 +2,6 @@ module.exports = [{
     entryPoint: "src/extension.tsx",
     indexHTMLTemplate: "index.html",
 }, {
-    entryPoint: "src/extension.tsx",
-    indexHTMLTemplate: "index.html",
-}, {
     entryPoint: "src/background.ts",
     outputFileNamePattern: "background-[name].js",
     serverMode: true,
